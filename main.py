36f7056e0bde2f2bad362ab17866a1164b73e6d9
Nothinormuch <nikhilsharma2006.all.on@gmail.com>
2023-09-19 06:44:48 +0530

    Change of name

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import side
+import SimpleCubes as sc
 def temp_creation():
-    c1=side.cube("Testcube1")
-    c2=side.cube("Testcube2")
-    side.select_column(c1,1,1,1).colour='Blue'
-    side.select_column(c2,1,2,1).colour='Red'
-    side.storage.exportt(c1,'','Testcube1.log')
-    side.storage.exportt(c2,'','Testcube2.log')
+    c1=sc.cube("Testcube1")
+    c2=sc.cube("Testcube2")
+    sc.select_column(c1,1,1,1).colour='Blue'
+    sc.select_column(c2,1,2,1).colour='Red'
+    sc.storage.exportt(c1,'','Testcube1.log')
+    sc.storage.exportt(c2,'','Testcube2.log')
 
 
-side.update_storage()
-oncube=side.chose_activecube(cubename='Testcube1',cubebag=side.storage.cubebag)
-side.select_column(oncube,1,1,1).colour='Green'
-side.storage.show_cubes(side.storage.cubebag)
+sc.update_storage()
+oncube=sc.chose_activecube(cubename='Testcube1',cubebag=sc.storage.cubebag)
+sc.select_column(oncube,1,1,1).colour='Green'
+sc.storage.show_cubes(sc.storage.cubebag)
