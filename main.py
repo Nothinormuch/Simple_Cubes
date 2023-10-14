0ddb5bad9ce5323b4c2d28554bae156145833ae1
Nothinormuch <140101409+Nothinormuch@users.noreply.github.com>
2023-10-14 02:20:55 +0000

    added print in main.py

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def temp_creation():
 sc.update_storage()
 oncube=sc.chose_activecube(cubename='Testcube1',cubebag=sc.storage.cubebag)
 sc.select_column(oncube,1,1,1).colour='Green'
-sc.storage.show_cubes(sc.storage.cubebag)
+print(sc.storage.show_cubes(sc.storage.cubebag))
